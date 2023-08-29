e479e3f191190682c87bd7c6c42ebd6bbf0feebf
葛西伸樹 <45755487+KNobuki@users.noreply.github.com>
2023-08-29 16:26:54 +0000

    Add memo list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,11 @@ import { styled } from "styled-components";
 
 export const App: FC = () => {
   const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([
+    "テスト1",
+    "テスト2",
+    "テスト3",
+  ]);
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
   const onClickAdd = () => {
@@ -14,10 +19,32 @@ export const App: FC = () => {
       <h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
   );
 };
 
 const SButton = styled.button`
   margin-left: 16px;
+`;
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
